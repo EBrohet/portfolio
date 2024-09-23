9afe3f93b43bcbf0a76d7c796fafcc9d706aa3b0
Emilie Brohet <emilie.brohet@gmail.com>
2024-09-23 17:20:03 +0200

    Add Contact Form by Mail

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,8 +9,6 @@ function Contact() {
         <div className="infos">
             <Form />
             <h2>Emilie Brohet</h2>
-            {/* <p>Tél : 06.79.72.50.42</p>
-            <p>Email : emilie.brohet@gmail.com</p> */}
             <a href='https://github.com/EBrohet'><img src={github} alt="logo Github" className='logo' /></a>
             <a href='https://www.linkedin.com/in/emilie-brohet-878537264'><img src={linkedin} alt="logo LinkedIn" className='logo' /></a>
         </div>
